11346c02f0dd7125ee09d98946fbf1a3fb3879f6
Anthony Ulloa M <48163743+Tony1702@users.noreply.github.com>
2020-09-25 19:22:06 -0600

    Update Servicio.java

--- Sala_Entrenamiento/src/model/Servicio.java
+++ Sala_Entrenamiento/src/model/Servicio.java
@@ -11,8 +11,27 @@ package model;
  */
 public class Servicio {
 
-    public Servicio() {
+    public String codigo;
+    public String descripcion;
+
+    public Servicio(String codigo, String descripcion) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
     }
-    
-    
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
