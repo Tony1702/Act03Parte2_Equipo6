4792fb410fb7327e6d72a223baafc08e87cc7b35
Anthony Ulloa M <48163743+Tony1702@users.noreply.github.com>
2020-09-25 19:22:48 -0600

    Update Persona.java

--- Sala_Entrenamiento/src/model/Persona.java
+++ Sala_Entrenamiento/src/model/Persona.java
@@ -11,7 +11,58 @@ package model;
  */
 public class Persona {
     
-    public Persona() {
+    protected String id;
+    protected String nombre;
+    protected String celular;
+    protected String correo;
+    protected Date fechaNacimiento;
+    
+    public Persona(String pId, String pNombre, String pCelular, String pCorreo, Date pFechaNacimiento) {
+        id = pId;
+        nombre = pNombre;
+        celular = pCelular;
+        correo = pCorreo;
+        fechaNacimiento = pFechaNacimiento;        
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
     }
     
 }
