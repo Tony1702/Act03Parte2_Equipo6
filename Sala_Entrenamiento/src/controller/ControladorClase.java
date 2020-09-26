5c3a635b9a4eb764456e03fd3c628445c0a1dc31
JAMares <49013556+JAMares@users.noreply.github.com>
2020-09-26 09:53:24 -0600

    Add files via upload

--- Sala_Entrenamiento/src/controller/ControladorClase.java
+++ Sala_Entrenamiento/src/controller/ControladorClase.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package controller;
+import java.util.ArrayList;
+import java.util.Collection;
+import model.Clase;
+import model.eDiaSemana;
+import model.Horario;
+import model.Instructor;
+import model.Servicio;
 
 /**
  *
  * @author Tony1
  */
 public class ControladorClase {
+    private Collection<Clase> clases;
+
+    public ControladorClase() {
+        this.clases = new ArrayList<Clase>();
+    }
+
+    public Collection<Clase> getClases() {
+        return clases;
+    }
+
+    public void setClases(Collection<Clase> clases) {
+        this.clases = clases;
+    }
+    
+    public String actualizarCalendario(eDiaSemana dia, Horario horario, int duracion, Instructor instructor, Servicio servicio){
+        return " ";
+    };
+    
+    public String listarClase(eDiaSemana dia){
+        return " ";
+    }
     
 }
--- Sala_Entrenamiento/src/controller/ControladorCliente.java
+++ Sala_Entrenamiento/src/controller/ControladorCliente.java
@@ -4,11 +4,36 @@
  * and open the template in the editor.
  */
 package controller;
+import java.util.ArrayList;
+import java.util.Collection;
+import model.Cliente;
+import model.eEstadoCliente;
 
 /**
  *
  * @author Tony1
  */
 public class ControladorCliente {
+    private Collection<Cliente> clientes;
+
+    public ControladorCliente() {
+        this.clientes = new ArrayList<Cliente>();
+    }
+
+    public Collection<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(Collection<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+    
+    public String listarClientes(eEstadoCliente estado) {
+        return " ";
+    }
+    
+    public String realizarPago(String id){
+        return "Pago realizado";
+    }
     
 }
