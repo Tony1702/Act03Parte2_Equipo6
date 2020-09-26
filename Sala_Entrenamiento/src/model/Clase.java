6db5ad452c287219535eb3e418c787ce8ef92f64
JAMares <49013556+JAMares@users.noreply.github.com>
2020-09-25 19:20:20 -0600

    Add files via upload

--- Sala_Entrenamiento/src/model/Clase.java
+++ Sala_Entrenamiento/src/model/Clase.java
@@ -4,11 +4,96 @@
  * and open the template in the editor.
  */
 package model;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  *
  * @author Tony1
  */
 public class Clase {
+    private Instructor intructor;
+    private Servicio servicio;
+    private int aforoClase;
+    private Collection<Cliente> asistencia;
+    private Horario horario;
+
+    /**
+     * 
+     * @param intructor
+     * @param servicio
+     * @param aforoClase
+     * @param horario 
+     */
+    public Clase(Instructor intructor, Servicio servicio, int aforoClase, Horario horario) {
+        this.intructor = intructor;
+        this.servicio = servicio;
+        this.aforoClase = aforoClase;
+        this.asistencia = new ArrayList<Cliente>();
+        this.horario = horario;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public Instructor getIntructor() {
+        return intructor;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public Servicio getServicio() {
+        return servicio;
+    }
+
+    public int getAforoClase() {
+        return aforoClase;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public Collection<Cliente> getAsistencia() {
+        return asistencia;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+
+    public void setIntructor(Instructor intructor) {
+        this.intructor = intructor;
+    }
+
+    /**
+     * 
+     * @param servicio 
+     */
+    public void setServicio(Servicio servicio) {
+        this.servicio = servicio;
+    }
+
+    /**
+     * 
+     * @param aforoClase 
+     */
+    public void setAforoClase(int aforoClase) {
+        this.aforoClase = aforoClase;
+    }
+
+    public void setAsistencia(Collection<Cliente> asistencia) {
+        this.asistencia = asistencia;
+    }
+
+    public void setHorario(Horario horario) {
+        this.horario = horario;
+    }
+    
+    
+    
     
 }
